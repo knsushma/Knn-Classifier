5d19b45a16a7dfe60d442a286af2d935b53dd23e
Sushma K N <sushmakn@Sushmas-MacBook-Pro.local>
2019-02-20 20:38:07 -0600

    All working code

--- HyperParamKnn.py
+++ HyperParamKnn.py
@@ -6,6 +6,7 @@ from numpy import *
 from collections import Counter
 import time
 from scipy.stats import mode
+import pandas as pd
 
 
 
@@ -73,26 +74,25 @@ class HyperParamKnn:
 
 if __name__ == '__main__':
 
-    k = 20
-    knn = HyperParamKnn(k, "./Resources/digits_train.json")
-    validateKnn = HyperParamKnn(k, "./Resources/digits_val.json")
-    testKnn = HyperParamKnn(k, "./Resources/digits_test.json")
+    k = 10
+    # knn = HyperParamKnn(k, "./Resources/digits_train.json")
+    # validateKnn = HyperParamKnn(k, "./Resources/digits_val.json")
+    # testKnn = HyperParamKnn(k, "./Resources/digits_test.json")
 
-    args = 2
+    knn = HyperParamKnn(k, "./Resources/votes_train.json")
+    validateKnn = HyperParamKnn(k, "./Resources/votes_val.json")
+    testKnn = HyperParamKnn(k, "./Resources/votes_test.json")
+
+    args = -2
 
     digitsTrainingSet = json.load(open(knn.inputFile))
     knn.features = np.array(digitsTrainingSet["metadata"]["features"])
     knn.metadata = np.array(digitsTrainingSet["data"])
     knn.labelTypes = knn.features[-1][1]
     knn.labels = knn.metadata[:, -1]
-    knn.mean = np.mean(knn.metadata,axis=0)
-    knn.sd = np.std(knn.metadata, axis=0)
-    knn.sd[knn.sd == 0.00] = 1.0
-
-    #stdTrainSet = knn.standardiseFeatures(np.asfarray(knn.metadata, float))
-    stdTrainSet = (np.asfarray(knn.metadata[:,0:-1], float) - knn.mean[0:-1])/knn.sd[0:-1]
-    stdTrainSet = np.column_stack([stdTrainSet,knn.metadata[:,-1]])
-
+    # knn.mean = np.mean(knn.metadata,axis=0)
+    # knn.sd = np.std(knn.metadata, axis=0)
+    # knn.sd[knn.sd == 0.00] = 1.0
 
     # noOfTrainDataSet = stdTrainSet.shape[0]
     # noOfTestDataSet = stdTestSet.shape[0]
@@ -100,7 +100,114 @@ if __name__ == '__main__':
 
     # pp(np.abs(stdTestSet[:, None, 0:-1] - stdTrainSet[None, :, 0:-1])[0][0])
     # sortedMatrix = np.sort(distanceMatrix, 1)[:, 2:, ]
-    if (args == 1):
+    if(args == -2):
+        digitsTestSet = json.load(open(testKnn.inputFile))
+        testKnn.features = np.array(digitsTestSet["metadata"]["features"])
+        testKnn.metadata = np.array(digitsTestSet["data"])
+        testKnn.labels = testKnn.metadata[:, -1]
+        noOfTestDataSet = testKnn.metadata.shape[0]
+
+
+        batchSize = knn.metadata.shape[0] / 10
+        for i in range(1, 11):
+            sliceSize = int(batchSize * i)
+
+            distanceMatrix = np.sum(testKnn.metadata[:, None, 0:-1] != knn.metadata[None, 0:sliceSize, 0:-1], -1)
+            ind = np.argsort(distanceMatrix, axis=1, kind='stablesort')[:, :k]
+            predictions = knn.metadata[0:sliceSize,:][ind, -1]
+            predictions = np.sort(predictions, axis=1)
+            flatResult = np.array(mode(predictions, axis=1)[0]).flatten()
+            count = np.sum(flatResult == np.array(testKnn.labels))
+            res = count.item() / noOfTestDataSet
+            print(sliceSize, end=",")
+            print(res)
+
+    elif (args == -1):
+
+        digitsValSet = json.load(open(validateKnn.inputFile))
+        validateKnn.features = np.array(digitsValSet["metadata"]["features"])
+        validateKnn.metadata = np.array(digitsValSet["data"])
+        validateKnn.labels = validateKnn.metadata[:, -1]
+
+        noOfValDataSet = validateKnn.metadata.shape[0]
+
+        accuracy = []
+        distanceMatrix = np.sum(np.absolute(validateKnn.metadata[:, None, 0:-1] != knn.metadata[None, :, 0:-1]), -1)
+        for kValue in range(1, knn.k+1):
+            #ind = np.argpartition(distanceMatrix, kValue, axis=1)[:, :kValue]
+            ind = np.argsort(distanceMatrix, axis=1, kind='stablesort')[:, :kValue]
+            predictions = knn.metadata[ind, -1]
+            result = np.array(mode(predictions, axis=1)[0])
+            flatResult = np.array(mode(predictions, axis=1)[0]).flatten()
+            count = np.sum(flatResult == np.array(validateKnn.labels))
+            res = count.item() / noOfValDataSet
+            print(kValue, end=",")
+            print(res)
+            accuracy.append([kValue, res])
+
+        npAccuracy = np.array(accuracy)
+        temp = npAccuracy[npAccuracy[:, 1].argsort()]
+        temp = temp[temp[:, 1] == npAccuracy.max(axis=0)[1]]
+        temp = temp[temp[:, 0].argsort()]
+        bestK = int(temp[0][0])
+        print(bestK)
+        # pp(npAccuracy.max(axis=0)[1])
+
+        newK = bestK
+
+        digitsTestSet = json.load(open(testKnn.inputFile))
+        testKnn.features = np.array(digitsTestSet["metadata"]["features"])
+        testKnn.metadata = np.array(digitsTestSet["data"])
+        testKnn.labels = testKnn.metadata[:, -1]
+
+        # Combine Train and Validation dataSet
+        trainValMetadata = np.concatenate((knn.metadata, validateKnn.metadata))
+        noOfTestDataSet = testKnn.metadata.shape[0]
+
+        distanceMatrix = np.sum(np.absolute(testKnn.metadata[:, None, 0:-1] != trainValMetadata[None, :, 0:-1]), -1)
+        #ind = np.argpartition(distanceMatrix, newK, axis=1)[:, :newK]
+        ind = np.argsort(distanceMatrix, axis=1, kind='stablesort')[:, :newK]
+        predictions = trainValMetadata[ind, -1]
+        predictions = np.sort(predictions, axis=1)
+
+        flatResult = np.array(mode(predictions, axis=1)[0]).flatten()
+        count = np.sum(flatResult == np.array(testKnn.labels))
+        res = count.item() / noOfTestDataSet
+        print(res)
+    elif (args == 0):
+
+        digitsTestSet = json.load(open(testKnn.inputFile))
+        testKnn.features = np.array(digitsTestSet["metadata"]["features"])
+        testKnn.metadata = np.array(digitsTestSet["data"])
+        testKnn.labels = testKnn.metadata[:, -1]
+
+        noOfTestDataSet = testKnn.metadata.shape[0]
+
+        distanceMatrix = np.count_nonzero(testKnn.metadata[:, None, 0:-1] != knn.metadata[None, :, 0:-1], -1)
+        ind = np.argsort(distanceMatrix, axis=1, kind='stablesort')[:, :k]
+        predictions = knn.metadata[ind, -1]
+        result = np.array(mode(predictions, axis=1)[0])
+
+        for index in range(predictions.shape[0]):
+            mapper = Counter(predictions[index].tolist())
+            for i in knn.labelTypes:
+                if (mapper.get(i) == None):
+                    print(0, end=",")
+                else:
+                    print(mapper.get(i), end=",")
+            print(result[index][0])
+        flatResult = np.array(mode(predictions, axis=1)[0]).flatten()
+        count = np.sum(flatResult == np.array(testKnn.labels))
+        res = count.item() / noOfTestDataSet
+        print(k, end=",")
+        print(res)
+    elif (args == 1):
+
+        # stdTrainSet = knn.standardiseFeatures(np.asfarray(knn.metadata, float))
+        stdTrainSet = (np.asfarray(knn.metadata[:, 0:-1], float) - knn.mean[0:-1]) / knn.sd[0:-1]
+        stdTrainSet = np.column_stack([stdTrainSet, knn.metadata[:, -1]])
+
+
         digitsTestSet = json.load(open(testKnn.inputFile))
         testKnn.features = np.array(digitsTestSet["metadata"]["features"])
         testKnn.metadata = np.array(digitsTestSet["data"])
@@ -129,7 +236,13 @@ if __name__ == '__main__':
         res = count.item() / noOfTestDataSet
         print(k, end=",")
         pp(res)
-    else:
+    elif (args == 2):
+
+        # stdTrainSet = knn.standardiseFeatures(np.asfarray(knn.metadata, float))
+        stdTrainSet = (np.asfarray(knn.metadata[:, 0:-1], float) - knn.mean[0:-1]) / knn.sd[0:-1]
+        stdTrainSet = np.column_stack([stdTrainSet, knn.metadata[:, -1]])
+
+
         digitsValSet = json.load(open(validateKnn.inputFile))
         validateKnn.features = np.array(digitsValSet["metadata"]["features"])
         validateKnn.metadata = np.array(digitsValSet["data"])
@@ -142,7 +255,7 @@ if __name__ == '__main__':
 
         accuracy = []
         distanceMatrix = np.sum(np.absolute(stdValSet[:, None, 0:-1] - stdTrainSet[None, :, 0:-1]), -1)
-        for kValue in range(knn.k+1):
+        for kValue in range(1,knn.k):
             ind = np.argpartition(distanceMatrix, kValue, axis=1)[:, :kValue]
             predictions = stdTrainSet[ind, -1].astype(int)
             #predictions  = np.sort(predictions, axis=1)
@@ -159,9 +272,70 @@ if __name__ == '__main__':
         temp = temp[temp[:, 1] == npAccuracy.max(axis=0)[1]]
         bestK = int(temp[0][0])
         pp(bestK)
-        pp(npAccuracy.max(axis=0)[1])
+        #pp(npAccuracy.max(axis=0)[1])
 
+        newK = bestK
 
+        digitsTestSet = json.load(open(testKnn.inputFile))
+        testKnn.features = np.array(digitsTestSet["metadata"]["features"])
+        testKnn.metadata = np.array(digitsTestSet["data"])
+        testKnn.labels = testKnn.metadata[:, -1]
+
+        # Combine Train and Validation dataSet
+        trainValMetadata = np.concatenate((knn.metadata,validateKnn.metadata))
+        mean = np.mean(trainValMetadata, axis=0)
+        sd = np.std(trainValMetadata, axis=0)
+        sd[sd == 0.00] = 1.0
+
+        # stdTrainSet = knn.standardiseFeatures(np.asfarray(knn.metadata, float))
+        stdTrainValSet = (np.asfarray(trainValMetadata[:, 0:-1], float) - mean[0:-1]) / sd[0:-1]
+        stdTrainValSet = np.column_stack([stdTrainValSet, trainValMetadata[:, -1]])
+
+        # stdTestSet = knn.standardiseFeatures(np.asfarray(testKnn.metadata, float))
+        stdTestSet = (np.asfarray(testKnn.metadata[:, 0:-1], float) - mean[0:-1]) / sd[0:-1]
+        stdTestSet = np.column_stack([stdTestSet, testKnn.metadata[:, -1]])
+        noOfTestDataSet = stdTestSet.shape[0]
+
+        distanceMatrix = np.sum(np.absolute(stdTestSet[:, None, 0:-1] - stdTrainValSet[None, :, 0:-1]), -1)
+        ind = np.argpartition(distanceMatrix, newK, axis=1)[:, :newK]
+        predictions = stdTrainValSet[ind, -1].astype(int)
+        predictions = np.sort(predictions, axis=1)
+
+        flatResult = np.array(mode(predictions, axis=1)[0]).flatten()
+        count = np.sum(flatResult == np.array(testKnn.labels))
+        res = count.item() / noOfTestDataSet
+        pp(res)
+    else:
+        digitsTestSet = json.load(open(testKnn.inputFile))
+        testKnn.features = np.array(digitsTestSet["metadata"]["features"])
+        testKnn.metadata = np.array(digitsTestSet["data"])
+        testKnn.labels = testKnn.metadata[:, -1]
+        noOfTestDataSet = testKnn.metadata.shape[0]
+
+        batchSize = knn.metadata.shape[0] / 10
+        for i in range(1, 11):
+            sliceSize = int(batchSize * i)
+            metaData = knn.metadata[0:sliceSize]
+
+            knn.mean = np.mean(metaData, axis=0)
+            knn.sd = np.std(metaData, axis=0)
+            knn.sd[knn.sd == 0.00] = 1.0
+
+            stdTrainSet = (np.asfarray(knn.metadata[0:sliceSize, 0:-1], float) - knn.mean[0:-1]) / knn.sd[0:-1]
+            stdTrainSet = np.column_stack([stdTrainSet, knn.metadata[0:sliceSize, -1]])
+
+            stdTestSet = (np.asfarray(testKnn.metadata[:, 0:-1], float) - knn.mean[0:-1]) / knn.sd[0:-1]
+            stdTestSet = np.column_stack([stdTestSet, testKnn.metadata[:, -1]])
+
+            distanceMatrix = np.sum(np.absolute(stdTestSet[:, None, 0:-1] - stdTrainSet[None, :, 0:-1]), -1)
+            ind = np.argpartition(distanceMatrix, k, axis=1)[:, :k]
+            predictions = stdTrainSet[ind, -1].astype(int)
+            predictions = np.sort(predictions, axis=1)
+            flatResult = np.array(mode(predictions, axis=1)[0]).flatten()
+            count = np.sum(flatResult == np.array(testKnn.labels))
+            res = count.item() / noOfTestDataSet
+            print(sliceSize, end=",")
+            pp(res)
 
 
 
---
+++ knnClassifierFindK.py
@@ -0,0 +1,143 @@
+from pprint import pprint as pp
+import json
+import math
+import numpy as np
+from numpy import *
+from collections import Counter
+
+
+
+class knnClassifierFindK:
+    def __init__(self, k , inputFilePath):
+        self.k = k
+        self.inputFile = inputFilePath
+        self.features = ""
+        self.metadata = ""
+        self.labels = []
+        self.mean = []
+        self.sd = []
+
+    def standardiseFeatures(self, dataSet):
+        for i in range(0, len(self.features)-1):
+            dataSet[:,i] = dataSet[:,i] - self.mean[i]
+            dataSet[:,i] = dataSet[:,i] / self.sd[i]
+        return dataSet
+
+    def returnBestK(self, nearestNeighbors, noOfTestDataSet):
+        testDataIndecies = np.unique((nearestNeighbors[:,0]).astype(int))
+        testSetLabels = self.metadata[:, -1]
+        accuracy = []
+        for k in range(1,self.k+1):
+            predictedLabels = []
+            neighbors = nearestNeighbors.copy()
+            for i in testDataIndecies:
+                temp = neighbors[neighbors[:,0]==i]
+                temp = temp[temp[:, 1].argsort()]
+                kNearestNeighbors = (temp[:k]).astype(int)
+                label = (Counter(kNearestNeighbors[:, 2]).most_common(1))[0][0]
+                predictedLabels.append(label)
+            count = 0
+            for index, label in enumerate(predictedLabels):
+                if (label == testSetLabels[index]):
+                    count += 1
+            accuracy.append([k,(count/noOfTestDataSet * 100)])
+        npAccuracy = np.array(accuracy)
+        temp = npAccuracy[npAccuracy[:, 1].argsort()]
+        temp = temp[temp[:,1]==npAccuracy.max(axis=0)[1]]
+        bestK = temp[0][0]
+        return bestK
+
+    def predictBestKHyperparameterNew(self, nearestNeighbors, noOfTestDataSet):
+        testDataIndecies = np.unique((nearestNeighbors[:, 0]).astype(int))
+        testSetLabels = self.metadata[:, -1]
+        accuracy = 0.0
+        bestK = 1
+        for k in range(1, self.k + 1):
+            predictedLabels = []
+            neighbors = nearestNeighbors.copy()
+            for i in testDataIndecies:
+                temp = neighbors[neighbors[:, 0] == i]
+                temp = temp[temp[:, 1].argsort()]
+                kNearestNeighbors = (temp[:k]).astype(int)
+                label = (Counter(kNearestNeighbors[:, 2]).most_common(1))[0][0]
+                predictedLabels.append(label)
+            count = 0
+            for index, label in enumerate(predictedLabels):
+                if (label == testSetLabels[index]):
+                    count += 1
+            curAccuracy = count / noOfTestDataSet * 100
+            if (curAccuracy > accuracy):
+                accuracy = curAccuracy
+            elif (curAccuracy == accuracy):
+                bestK = k - 1
+                break
+            else:
+                bestK = k
+                break
+        pp(bestK)
+        return bestK
+
+    def predictBestKHyperparameter(self, nearestNeighbors, noOfTestDataSet):
+        testDataIndecies = np.unique((nearestNeighbors[:,0]).astype(int))
+        testSetLabels = self.metadata[:, -1]
+
+        accuracy = []
+        for k in range(1, self.k + 1):
+            predictedLabels = []
+            # neighbors = nearestNeighbors.copy()
+            for i in testDataIndecies:
+                temp = nearestNeighbors[nearestNeighbors[:, 0] == i]
+                temp = temp[temp[:, 1].argsort()]
+                kNearestNeighbors = (temp[:k]).astype(int)
+                label = (Counter(kNearestNeighbors[:, 2]).most_common(1))[0][0]
+                predictedLabels.append(label)
+            count = 0
+            for index, label in enumerate(predictedLabels):
+                if (label == testSetLabels[index]):
+                    count += 1
+            acc = (count / noOfTestDataSet)
+            print(k, end=",")
+            pp(acc)
+            accuracy.append([k, acc])
+        npAccuracy = np.array(accuracy)
+        temp = npAccuracy[npAccuracy[:, 1].argsort()]
+        temp = temp[temp[:, 1] == npAccuracy.max(axis=0)[1]]
+        bestK = temp[0][0]
+        pp(bestK)
+        pp(npAccuracy.max(axis=0)[1])
+        return bestK
+
+
+if __name__ == '__main__':
+
+    knn = knnClassifierFindK(3, "./Resources/digits_train.json")
+    validateKnn = knnClassifierFindK(3, "./Resources/digits_val.json")
+
+    digitsTrainingSet = json.load(open(knn.inputFile))
+    knn.features = np.array(digitsTrainingSet["metadata"]["features"])
+    knn.metadata = np.array(digitsTrainingSet["data"])
+    knn.labels = knn.features[-1][1]
+
+    noOfFeatures = len(knn.features)-1
+    knn.mean = np.mean(knn.metadata,axis=0)
+    knn.sd = np.std(knn.metadata, axis=0)
+
+    knn.sd[knn.sd == 0.00] = 1.0
+    stdTrainSet = knn.standardiseFeatures(np.asfarray(knn.metadata, float))
+
+    digitsTestSet = json.load(open(validateKnn.inputFile))
+    validateKnn.features = np.array(digitsTestSet["metadata"]["features"])
+    validateKnn.metadata = np.array(digitsTestSet["data"])
+    stdTestSet = knn.standardiseFeatures(np.asfarray(validateKnn.metadata, float))
+
+    noOfTestDataSet = stdTestSet.shape[0]
+    noOfTrainDataSet = stdTrainSet.shape[0]
+
+    #noOfTestDataSet = 20
+    predictedLabel = []
+    nearestNeighbors = []
+    for testIndex in range(0, noOfTestDataSet):
+        for trainIndex in range(0, noOfTrainDataSet):
+            nearestNeighbors.append([testIndex, (np.abs(stdTrainSet[trainIndex,0:-1]-stdTestSet[testIndex,0:-1])).sum(), stdTrainSet[trainIndex,-1]])
+    bestK = validateKnn.predictBestKHyperparameter(np.array(nearestNeighbors), noOfTestDataSet)
+    pp("Best K : {}".format(bestK))
